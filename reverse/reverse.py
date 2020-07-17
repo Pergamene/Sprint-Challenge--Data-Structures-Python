66d3610dcee90abcd926248be6d154fc9279707d
Pergamene <smartz373@gmail.com>
2020-07-17 10:00:01 -0600

    reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,42 +1,47 @@
 class Node:
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
 
-    def get_value(self):
-        return self.value
+  def get_value(self):
+    return self.value
 
-    def get_next(self):
-        return self.next_node
+  def get_next(self):
+    return self.next_node
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+  def set_next(self, new_next):
+    self.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    self.head = None
 
-    def add_to_head(self, value):
-        node = Node(value)
+  def add_to_head(self, value):
+    node = Node(value)
+    if self.head is not None:
+      node.set_next(self.head)
+    self.head = node
 
-        if self.head is not None:
-            node.set_next(self.head)
+  def contains(self, value):
+    if not self.head:
+      return False
+    current = self.head
+    while current:
+      if current.get_value() == value:
+        return True
+      current = current.get_next()
+    return False
 
-        self.head = node
+  def reverse_list(self, node, prev):
+    if node is None:
+      return
+    else:
+      self._reverse_list_recursive(node, prev)
 
-    def contains(self, value):
-        if not self.head:
-            return False
-
-        current = self.head
-
-        while current:
-            if current.get_value() == value:
-                return True
-
-            current = current.get_next()
-
-        return False
-
-    def reverse_list(self, node, prev):
-        pass
+  def _reverse_list_recursive(self, node, prev):
+    next_node = node.get_next()
+    node.next_node = prev
+    if next_node is None:
+      self.head = node
+    else:
+      self._reverse_list_recursive(next_node, node)
