8b2c5dada708d62f63414bd9d615e03c0df238b3
Pergamene <smartz373@gmail.com>
2020-07-17 09:23:05 -0600

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+  def __init__(self, capacity):
+    self.buffer = [None] * capacity
+    self.oldest = None
+    self.length = 0
+    self.capacity = capacity
 
-    def append(self, item):
-        pass
+  def append(self, item):
+    if self.oldest is None:
+      self.buffer[0] = item
+      self.oldest = 0
+      self.length = 1
+    elif self.length < len(self.buffer):
+      self.buffer[self.length] = item
+      self.length += 1
+    else:
+      self.buffer[self.oldest] = item
+      self.oldest = (self.oldest + 1) % self.length
 
-    def get(self):
-        pass
+  def get(self):
+    if self.length == 0:
+      return []
+    elif self.length < len(self.buffer):
+      return self.buffer[:self.length]
+    return self.buffer
